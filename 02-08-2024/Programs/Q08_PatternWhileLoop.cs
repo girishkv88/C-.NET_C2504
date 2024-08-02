6d9eac05a6ca414aaf68c8e33217de73a4473dac
Girish k v <73742657+girishkv88@users.noreply.github.com>
2024-08-02 21:13:59 +0530

    Create Q08_PatternWhileLoop.cs

---
+++ 02-08-2024/Programs/Q08_PatternWhileLoop.cs
@@ -0,0 +1,45 @@
+
+using System;
+
+class Q08
+{
+
+    static void PrintNumTriangleMirroredRightAngle(int N)
+    {
+        int I = 1;
+        while(I <= N)
+        {
+            
+            int j = 1;
+            while (j <= N-I)
+            {
+                Console.Write("  ");
+                j++;
+            }
+
+            int k = 1;
+            while (k <= I)
+            {
+                Console.Write($"{k} ");
+                k++;
+            }
+            I = I + 1;
+            Console.WriteLine();
+        }
+    }
+
+    static void TestPrintNumTriangleMirroredRightAngle()
+    {
+        Console.Write("Enter number of lines:");
+        int N = int.Parse(Console.ReadLine());
+        PrintNumTriangleMirroredRightAngle(N);
+    }
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("-----TestPrintNumTriangleMirroredRightAngle while loop-----");
+        TestPrintNumTriangleMirroredRightAngle();
+        Console.WriteLine("-----End TestPrintNumTriangleMirroredRightAngle while loop-----");
+        Console.WriteLine("Press any key to contine...");
+        Console.ReadKey();
+    }
+}
