ab20a630730cb62e95ccc0106a7b690f42b0e3e6
Girish k v <73742657+girishkv88@users.noreply.github.com>
2024-08-02 11:02:08 +0530

    Create Q01_ForLoop.cs

---
+++ 02-08-2024/Programs/Q01_ForLoop.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q01
+{
+    internal class Program
+    {
+        static void PrintSeries10_12_14(int numOfTerms)
+        {
+            int term = 10;
+            for (int I = 1; I <= numOfTerms; I++)
+            {
+                Console.Write($"{term} ");
+                term = term + 2;
+            }
+        }
+        // input=5, output=10 12 14 16 18
+        // input=8, output=10 12 14 16 18 20 22 24
+        // input=3, output=10 12 14
+        static void TestPrintSeries10_12_14()
+        {
+            Console.Write("Enter number of terms:");
+            int numOfTerms = int.Parse(Console.ReadLine());
+            Console.Write("Series: ");
+            PrintSeries10_12_14(numOfTerms);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) //user: p
+        {
+            Console.WriteLine("--------------TestPrintSeries10_12_14..using for loop.--------------");
+            TestPrintSeries10_12_14();
+            Console.WriteLine("--------------End TestPrintSeries10_12_14...using for loop--------------");
+            Console.WriteLine("Press any key to contine...");
+            Console.ReadKey();
+        }
+    }
+}
